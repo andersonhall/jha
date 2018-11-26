c148273238cf56c151826a91f222ed5d9bc30a78
Andy Hall <andersonhall@gmail.com>
2018-11-26 12:25:23 -0600

    Add transition to mobile nav

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu-btn');
 
-menu.style.visibility = 'hidden';
 menuBtn.addEventListener('click', toggleMenu);
 
+//function toggleMenu() {
+//  if (menu.style.visibility === 'hidden') {
+//    menu.style.visibility = 'visible';
+//  } else if (menu.style.visibility === 'visible') {
+//    menu.style.visibility = 'hidden';
+//  }
+//}
+
 function toggleMenu() {
-  console.log('clicking')
-  if (menu.style.visibility === 'hidden') {
-    console.log('v');
-    menu.style.visibility = 'visible';
-  } else if (menu.style.visibility === 'visible') {
-    console.log('h');
-    menu.style.visibility = 'hidden';
-  }
+ menu.classList.toggle('show');
 }
