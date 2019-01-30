280e23f3ae8891be828fa70f7857531e2cfb0236
Andy <andersonhall@gmail.com>
2019-01-30 01:22:28 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,2 @@
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu-btn');
-
-menuBtn.addEventListener('click', toggleMenu);
-
-//function toggleMenu() {
-//  if (menu.style.visibility === 'hidden') {
-//    menu.style.visibility = 'visible';
-//  } else if (menu.style.visibility === 'visible') {
-//    menu.style.visibility = 'hidden';
-//  }
-//}
-
-function toggleMenu() {
- menu.classList.toggle('show');
-}
+// Get year
+$("#year").text(new Date().getFullYear());
