bd22d4d12e14f2efe5a540074cf3f81d23228173
Andy Hall <40648667+andersonhall@users.noreply.github.com>
2019-02-02 11:20:11 -0600

    Slick slider

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // Get year
 $('#year').text(new Date().getFullYear());
+
+// Init slick slider for testimonials
+$(document).ready(function() {
+  $('.slick-testimonials').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false
+  });
+});
